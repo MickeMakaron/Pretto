3d007ea4c36cdb0c2c6cac83cdf34318f40e25ca
MickeMakaron <MickeMakaron@users.noreply.github.com>
2014-04-28 16:57:28 +0200

    fixed

--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -1,8 +1,2 @@
-<h1>Pretto - a basic MVC</h1>
-<p>Pretto is...</p>
-
-<h2>Installation</h2>
-<p>Download the latest build at <a href='https://github.com/MickeMakaron/Pretto/tree/v0.15'>github</a> and extract the contents to your site's root.</p>
-<p>Allow writing to the data folder at "site/data" (777) and the database file .ht.sqlite (666). If you want to use the standard grid theme, you must also allow writing to "themes/grid" (777).</p>
-<p>For some web servers you must rewrite base URL by adding a RewriteBase in .htaccess.</p>
-<p>Lastly, go to your site through your browser and press the installation link. That's it!</p>
+<h1>Thank you for using Pretto!</h1>
+<p>To finalize installation, go to <a href='<?=create_url('modules/install')?>'>modules/install</a></p>
