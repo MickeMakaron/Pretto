f1f9ce3034799326263eec3d9a6444e25f5c4ed4
MickeMakaron <MickeMakaron@users.noreply.github.com>
2014-04-28 16:48:10 +0200

    git sucks

--- kmom07_report.php
+++
@@ -1,7 +0,0 @@
-<h2>kmom07</h2>
-<p><a href='http://www.student.bth.se/~mihe14/phpmvc/kmom07/me/'>Me-sida</a></p>
-<p><a href='http://www.student.bth.se/~mihe14/phpmvc/kmom07/'>Index</a></p>
-<p><a href='http://www.student.bth.se/~mihe14/phpmvc/kmom07/modules'>Modules</a></p>
-<p><a href='https://github.com/MickeMakaron/Pretto/tree/v0.15'>Git (v0.15)</a></p>
-
-<p></p>
