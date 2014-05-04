04c2dc47496ffe478e68768aa0b9d5e0f1cdf70f
MickeMakaron <MickeMakaron@users.noreply.github.com>
2014-05-04 16:14:26 +0200

    .

---
+++ src/CCIndex/index.tpl.php
@@ -0,0 +1,4 @@
+<h1>Thank you for using Pretto!</h1>
+
+<p><a href='<?=create_url('install')?>'>Click here to view installation instructions.</a></p>
+<p>If you get 404'd, you probably need to edit the RewriteBase rule in the <code>.htaccess</code> file.</p>
